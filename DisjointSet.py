71e1becdbc31dbe666e58673665cbcb0558a985f
Satwika <155884969+satwika-55@users.noreply.github.com>
2024-09-12 22:41:49 +0530

    Create DisjointSet.py

---
+++ DisjointSet.py
@@ -0,0 +1,40 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.rank = [0] * (n + 1)
+        self.parent = list(range(n + 1))
+        self.size = [1] * (n + 1)
+
+    def findUPar(self, node):
+        if node == self.parent[node]:
+            return node
+        ulp = self.findUPar(self.parent[node])
+        self.parent[node] = ulp
+        return self.parent[node]
+
+    def unionByRank(self, u, v):
+        ulp_u = self.findUPar(u)
+        ulp_v = self.findUPar(v)
+        if ulp_u == ulp_v:
+            return
+        if self.rank[ulp_u] < self.rank[ulp_v]:
+            self.parent[ulp_u] = ulp_v
+        elif self.rank[ulp_v] < self.rank[ulp_u]:
+            self.parent[ulp_v] = ulp_u
+        else:
+            self.parent[ulp_v] = ulp_u
+            self.rank[ulp_u] += 1
+
+    def unionBySize(self, u, v):
+        ulp_u = self.findUPar(u)
+        ulp_v = self.findUPar(v)
+        if ulp_u == ulp_v:
+            return
+        if self.size[ulp_u] < self.size[ulp_v]:
+            self.parent[ulp_u] = ulp_v
+            self.size[ulp_v] += self.size[ulp_u]
+        else:
+            self.parent[ulp_v] = ulp_u
+            self.size[ulp_u] += self.size[ulp_v]
+'''
+this is the code snippet for the entire disjoint set data structure
+'''
